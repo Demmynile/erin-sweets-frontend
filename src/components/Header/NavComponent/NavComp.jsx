fc302d72eec7fc00beb2ab8fcb47ed8693461287
Demmynile <balogun80@gmail.com>
2024-03-02 10:43:27 +0000

    new changes in the application

--- src/components/Header/NavComponent/NavComp.jsx
+++ src/components/Header/NavComponent/NavComp.jsx
@@ -9,12 +9,12 @@ const NavComp = ({isMobile }) => {
        <div className={isMobile ? 'mob-container' : 'main-container'}>
           <div className="sub-container">
              <div className='nav-list '>
-               <li className='easter'><a href="https://erin-sweets-frontend.vercel.app/category/5">Easter</a></li>  
-               <li  className='mother'><a href="https://erin-sweets-frontend.vercel.app/category/6">Mother's Day</a></li>  
-               <li  className='afternoon'><a href="https://erin-sweets-frontend.vercel.app/category/8">Afternoon Tea</a></li>  
+               <div className='easter'><a href="https://erin-sweets-frontend.vercel.app/category/5">Easter</a></div>  
+               <div  className='mother'><a href="https://erin-sweets-frontend.vercel.app/category/6">Mother's Day</a></div>  
+               <div className='afternoon'><a href="https://erin-sweets-frontend.vercel.app/category/8">Afternoon Tea</a></div>  
                {/* <li  className='hamper'>Hamper and Gift Boxes</li>   */}
-               <li  className='gift'>Gift Ideas</li>     
-               <li  className='cakes'><a href="https://erin-sweets-frontend.vercel.app/category/7">Cakes</a></li>  
+               <div  className='gift'>Gift Ideas</div>     
+               <div  className='cakes'><a href="https://erin-sweets-frontend.vercel.app/category/7">Cakes</a></div>  
                {/* <li  className='chocolates'>Chocolates</li>   */}
                {/* <li  className='tea'>Tea and Coffee</li>  
                <li  className='corporate'>Corporate Gift</li>   */}
